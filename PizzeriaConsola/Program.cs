e581d2a83386b8a8c94b0c44184cc76a582ae979
jhonduarte79 <jfduarte79@gmail.com>
2022-08-29 17:50:16 -0500

    unificado BBDD y CRUD

--- PizzeriaConsola/Program.cs
+++ PizzeriaConsola/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using PizzeriaDominio;
-
+using PizzeriaPersistencia;
 
 namespace PizzeriaConsola
 {
     class Program
     {
+        private static IRepositorioTb_cliente _repoTb_cliente = new RepositorioTb_cliente(new PizzeriaPersistencia.AppContext());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("successful conexion!");
+            CrearCliente();
+        }
+        private static void CrearCliente()
+        {
+            var cliente = new Cliente
+            {
+                Nombres = "jhon",
+                Apellidos = "duarte",
+                Email = "jfduarte79@gmail.com",
+                Edad = 43,
+                Telefono = "345672",
+                Direccion = "cali, valle, colombia",
+                Password = "3421"
+
+            };
+            _repoTb_cliente.CrearCliente(cliente);
         }
     }
 }
